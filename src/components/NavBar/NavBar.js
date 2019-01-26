9c7344dbe5407ae66a2c6d8b458978f730c27d62
seabasshoang <sylviahoang@hotmail.co.uk>
2019-01-26 15:55:41 +0000

    created nav bar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+    return (
+        <div class="navBox">
+            <div class="title">PRECIOUS PIECE</div>
+            <ul class="navItems">
+                <li class="navItem1">sunglasses</li>
+                <li class="navItem2">glasses</li>
+                <li class="navItem3">campaigns</li>
+            </ul>
+        </div>
+        
+    );
+}
+
+export default NavBar;
